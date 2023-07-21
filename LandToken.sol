9a5cce52a27019188fcf35107d89143e262a9c9b
IdolonInMachina <Cooperbar@gmail.com>
2023-07-21 15:28:08 +1000

    Working smart contract

--- LandToken.sol
+++ LandToken.sol
@@ -2,26 +2,63 @@
 
 pragma solidity >=0.7.0 < 0.9.0;
 
-import "./node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";        
-import "@openzeppelin/contracts/drafts/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract LandToken is ERC721 {
+
+contract LandToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721Full("LandToken", "LAND") public {
+    constructor() ERC721("LandToken", "LAND") {
     }
 
-    // Things we need
+    function mintLandToken(address landOwner, string memory newTokenURI) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(landOwner, newTokenId);
+        _setTokenURI(newTokenId, newTokenURI);
 
-    // Mint function
-    // - Called by the backend once it has created the token uri json
-    // - Mints the token and assigns it to the user
+        return newTokenId;
+    }
 
+    // Note: _burn can only be called by the owner of the token or an "approved operator".
+    // Let's see how we need to handle this, considering a user needs to burn an old token to merge two Land Tokens.
+    function burnLandToken(uint256 tokenId) public {
+        _burn(tokenId);
+    }
 
-    string json = createLandTokenMetadata(lat1, long1, lat2, long2, lat3, long3);
-    string path = generateURLPath(json);
-    uint256 tokenId = LandToken.mint(recipient, path);
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
 }
