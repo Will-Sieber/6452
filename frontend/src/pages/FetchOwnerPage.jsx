63d5a3a668387daadd416ab597b48966c83572c1
Will-Sieber <wsieber455@gmail.com>
2023-07-25 01:31:35 +1000

    work on user tokens

--- frontend/src/pages/FetchOwnerPage.jsx
+++ frontend/src/pages/FetchOwnerPage.jsx
@@ -1,10 +1,10 @@
 // FetchOwnerPage.js
 import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
+import Web3, { errors } from 'web3';
 import ActionAreaCard from '../components/Card';
 import jsondata from "../ABI.json"
 import './FetchOwnerPage.css'; // Import the CSS file
-import {TOKEN_CONTRACT_ADDRESS as CONTRACT_ADDRESS} from '../config'
+import {TOKEN_CONTRACT_ADDRESS as CONTRACT_ADDRESS} from '../config';
 const ABI = jsondata;
 //const CONTRACT_ADDRESS = localStorage.getItem("contractAddress")
 
@@ -12,7 +12,9 @@ const FetchOwnerPage = () => {
   const [contract, setContract] = useState(null);
   const [ownerAddress, setOwnerAddress] = useState('');
   const [URI, setURI] = useState('')
-  const [jsonData, setJsonData] = useState(null);
+  let index = 0;
+  let user_token_ids = [];
+  let account = null;
 
   useEffect(() => {
     let isMounted = true;
@@ -21,47 +23,18 @@ const FetchOwnerPage = () => {
     const initializeWeb3 = async () => {
       if (typeof window.ethereum !== 'undefined') {
         web3 = new Web3(window.ethereum);
+        fetchAccount(web3).then((address) => {
+          account = address;
+        })
+
         try {
           await window.ethereum.enable();
           const contractInstance = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
           setContract(contractInstance);
-
-          // Fetch the owner address
-          contractInstance.methods
-            .owner()
-            .call()
-            .then((owner) => {
-              if (isMounted) {
-                setOwnerAddress(owner);
-              }
-            })
-            .catch((error) => {
-              console.error('Error getting owner address:', error);
-            });
-
-            // Fetch the token URI
-            contractInstance.methods
-            .tokenURI(1)
-            .call()
-            .then((URI) => {
-                if (isMounted) {
-                setURI(URI);
-
-                // Fetch the JSON data from the URI
-                fetch(URI)
-                    .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                    })
-                    .then(data => setJsonData(data))
-                    .catch(error => console.error('Error fetching JSON data:', error));
-                }
-            })
-            .catch((error) => {
-                console.error('Error getting token URI:', error);
-            });
+          
+          console.log("bruh")
+          findTokenRecursive(contractInstance);
+  
 
         } catch (error) {
           console.error('Error initializing web3:', error);
@@ -79,20 +52,40 @@ const FetchOwnerPage = () => {
     };
   }, []);
 
-  const allJsons = [
-    jsonData,
-    jsonData,
-    jsonData,
-    jsonData,
-    jsonData,
-    jsonData,
-  ]
+  const findTokenRecursive = async (contract) => {
+    if (!contract) {
+      console.log("no contract??");
+      return;
+    }
+
+    contract.methods.tokenOfOwnerByIndex(account, index).call((error, result) => {
+      if (!error) {
+        console.log(result.toNumber());
+        user_token_ids.push(result.toNumber());
+        index++;
+        findTokenRecursive(contract);
+      } else {
+        console.log("oopsie wwoopsie")
+        return;
+      }
+    })
+  };
+
+
+  const fetchAccount = async (web3) => {
+    const accounts = await web3.eth.getAccounts();
+      if (accounts > 0) {
+        const userAddress = accounts[0];
+        return userAddress;
+      }
+  }
+
+  const allJsons = [];
 
   return (
     <div>
       <h2>Owner Address</h2>
       {ownerAddress && <p>Owner Address: {ownerAddress}</p>}
-      {URI && <p>URI: {URI}</p>}
       <div className='cardcontainer'>
         {allJsons.map((item) => (
           <ActionAreaCard
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css'; // Import the CSS file
+import { BACKEND_URL as BASE_URL } from '../config';
 
 const HomePage = () => {
   const [contractAddress, setContractAddress] = useState('');
@@ -14,6 +15,26 @@ const HomePage = () => {
     // Store the contract address in local storage
     localStorage.setItem('contractAddress', contractAddress);
   };
+
+  const fetchAllTokens = async () => {
+    return fetch(BASE_URL+'/all')
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Oooer am not good with computer plz help");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) =>  {
+      console.error("bad thing uh oh", error);
+    });
+  };
+
+  // fetchAllTokens().then((allTokens) => {
+  //  console.log(allTokens);
+  //});
 
   return (
     <div className="container">
