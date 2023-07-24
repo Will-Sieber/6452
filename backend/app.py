7c5b9887ecca2600781d03243af49ac8f940b3cb
IdolonInMachina <Cooperbar@gmail.com>
2023-07-25 02:24:21 +1000

    Adds backend for managing tokens Includes submitting files, viewing files, approving and rejecting tokens

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,26 @@
-from flask import Flask, request, Response, render_template
+from flask import Flask, request, Response, render_template, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+from werkzeug.utils import secure_filename
 from shapely.geometry import Point, LinearRing
 from shapely.geometry.polygon import Polygon
 from shapely.validation import explain_validity
 import os
 import json
 
+UPLOAD_FOLDER = 'hosted-files/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
 CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['UPLOAD_FOLDER'] = os.path.join(basedir, UPLOAD_FOLDER)
 db = SQLAlchemy(app)
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.mkdir(app.config['UPLOAD_FOLDER'])
 
 ## Models
 
@@ -28,7 +35,7 @@ class Point(db.Model):
     
 class Area(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    points = db.relationship('Point', backref='area', lazy=True)
+    points = db.relationship('Point', backref='area', lazy=True, cascade='all, delete-orphan')
     boundary_id = db.Column(db.Integer, db.ForeignKey('token.id'), nullable=True)
     hole_id = db.Column(db.Integer, db.ForeignKey('token.id'), nullable=True)
     
@@ -38,8 +45,10 @@ class Area(db.Model):
 class Token(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pending = db.Column(db.Boolean, default=True)
-    boundary = db.relationship('Area', backref='token_boundary', lazy=True, foreign_keys=[Area.boundary_id], uselist=False)
-    holes = db.relationship('Area', backref='token_holes', lazy=True, foreign_keys=[Area.hole_id])
+    initial_owner = db.Column(db.String(255), nullable=True, default="0x0000000000000000000000000000000000000000")
+    boundary = db.relationship('Area', backref='token_boundary', lazy=True, foreign_keys=[Area.boundary_id], uselist=False, cascade='all, delete-orphan')
+    holes = db.relationship('Area', backref='token_holes', lazy=True, foreign_keys=[Area.hole_id], cascade='all, delete-orphan')
+    files = db.relationship('File', backref="token", lazy=True, cascade='all, delete-orphan')
     
     def __repr__(self):
         return '<Token %r>' % self.id
@@ -59,6 +68,18 @@ class Token(db.Model):
     @property
     def LinearRing(self):
         return LinearRing([(p.lat, p.lon) for p in self.boundary.points])
+    
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    token_id = db.Column(db.Integer, db.ForeignKey('token.id'), nullable=False)
+
+    def __repr__(self):
+        return '<File %r>' % self.filename
+    
+    @property
+    def usable_filename(self):
+        return f"{self.token.id}-{self.id}-{self.filename}"
 
 
 
@@ -177,6 +198,140 @@ def check_merge():
         "valid": len(not_touching) == 0,
         "not_touching": list(not_touching)
     }
+
+# File upload/download
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/files/upload', methods=['POST'])
+def upload_file():
+    data = request.form.to_dict()
+    if 'file' in request.files:
+        file = request.files['file']
+        if 'reference_id' not in data:
+            return {
+                "success": False,
+                "message": "No reference_id provided. Please provide a reference_id for an existing token, or set reference_id to None to create a new token."
+            }, 400
+        token = Token.query.filter_by(id=data['reference_id']).first()
+        if token is None:
+            return "Token not found", 404
+        if data['reference_id'] is None:
+            if 'submitter_address' not in data:
+                return {
+                    "Success": False,
+                    "message": "No submitter_address provided. Please provide the address for the new token to be minted to once it has been approved."
+                }, 400
+            token = Token(pending=True)
+            db.session.add(token)
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file and file.filename != '' and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_obj = File(filename=filename, token=token)
+            db.session.add(file_obj)
+            db.session.commit()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_obj.usable_filename))
+            return {
+                    "success": True,
+                    "reference_id": None,
+                    "filename": file_obj.usable_filename,
+                }, 200
+    else:
+        print((request.__dict__))
+        return "No file detected", 400
+
+@app.route('/files/list', methods=['GET'])
+def get_files():
+    token_id = request.args.get('reference_id')
+    if not token_id:
+        return "Reference ID not provided", 400
+    token = Token.query.filter_by(id=token_id).first()
+    if token is None:
+        return "Token not found", 404
+    return {
+        "filenames": [file.usable_filename for file in token.files]
+    }
+
+@app.route('/files/download/<filename>', methods=['GET'])
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+# Approval stuff
+
+@app.route('/approve', methods=['POST'])
+def approve():
+    data = request.get_json()
+    token = Token.query.filter_by(id=data['reference_id']).first()
+    if token is None:
+        return "Token not found", 404
+    if token.boundary is None:
+        return "Token has no boundary", 422
+    token.pending = False
+    db.session.commit()
+    # TODO: NOW, GO MINT THE TOKEN
+    return {
+        "success": True
+    }
+
+@app.route('/set', methods=['PATCH'])
+def set_token():
+    # NOTE: this will completely replace the areas that are currently in the token
+    data = request.get_json()
+    token = Token.query.filter_by(id=data['reference_id']).first()
+    if token is None:
+        return "Token not found", 404
+    points = [(float(p['lat']), float(p['lon'])) for p in data['points']]
+    holes = [[(float(p['lat']), float(p['lon'])) for p in hole['points']] for hole in data['holes']]
+    success, conflicts = do_check(points, holes)
+    if not success:
+        return {"success": False, "conflicts": conflicts}, 409
+    token.boundary.delete()
+    for hole in token.holes:
+        hole.delete()
+    db.session.commit()
+    if len(points) > 0:
+        boundary = Area()
+        for point in data['points']:
+            new_point = Point(area=boundary, lat=point['lat'], lon=point['lon'])
+            db.session.add(new_point)
+        token.boundary = boundary
+    if len(data['holes']) > 0:
+        for hole in data['holes']:
+            new_hole = Area()
+            for point in hole['points']:
+                new_point = Point(area=new_hole, lat=point['lat'], lon=point['lon'])
+                db.session.add(new_point)
+            token.holes.append(new_hole)
+    db.session.commit()
+    return {
+        "success": True,
+        "reference_id": token.id,
+        "uri": f"{request.host_url}uri/token-{token.id}.json"
+    }
+    
+
+@app.route('/reject', methods=['POST'])
+def reject():
+    data = request.get_json()
+    token = Token.query.filter_by(id=data['reference_id'], pending=True).first()
+    if token is None:
+        return "Token not found", 404
+    token.delete()
+    db.session.commit()
+    return {
+        "success": True
+    }
+
+@app.route('/list/unapproved', methods=['GET'])
+def list_unapproved():
+    tokens = Token.query.filter_by(pending=True).all()
+    return {
+        "reference_ids": [token.id for token in tokens]
+    }
+
     
 @app.route("/uri/token-<token_id>.json")
 def get_item(token_id: int):
