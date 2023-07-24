17d058d13fdd559ef5ddf1b8568d4cb4421db7ee
IdolonInMachina <Cooperbar@gmail.com>
2023-07-24 17:21:08 +1000

    Updates contract address and adds token-3

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const TOKEN_CONTRACT_ADDRESS = '0xdf7e1a2bbf3213ffe626cbad33086556e332a070';
+export const TOKEN_CONTRACT_ADDRESS = '0x64f75ce32e93a149b9373d2070c3b8b22f069e81';
 export const BACKEND_URL = 'https://honeybee-ethical-radically.ngrok-free.app';
