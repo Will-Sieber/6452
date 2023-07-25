ec5120eaa36a29b6a79d946e7db993b5f025369f
Will-Sieber <wsieber455@gmail.com>
2023-07-25 16:24:02 +1000

    Update homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,20 +1,10 @@
 // HomePage.js
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './HomePage.css'; // Import the CSS file
 import { BACKEND_URL as BASE_URL } from '../config';
 
 const HomePage = () => {
-  const [contractAddress, setContractAddress] = useState('');
-
-  const handleContractAddressChange = (event) => {
-    setContractAddress(event.target.value);
-  };
-
-  const handleContractAddressSubmit = () => {
-    // Store the contract address in local storage
-    localStorage.setItem('contractAddress', contractAddress);
-  };
+  const [numTokens, setnumTokens] = useState(0);
 
   const fetchAllTokens = async () => {
     return fetch(BASE_URL+'/all')
@@ -32,22 +22,37 @@ const HomePage = () => {
     });
   };
 
-  // fetchAllTokens().then((allTokens) => {
-  //  console.log(allTokens);
-  //});
+  fetchAllTokens().then( async (allTokens) => {
+    const response = await allTokens;
+    setnumTokens(response.tokens.length);
+  });
+
+  const handleMerge = () => {
+    console.log("Merge")
+  }
+
+  const handleSplit = () => {
+    console.log("Split")
+  }
+
+  const handleMint = () => {
+    console.log("Mint")
+  }
 
   return (
     <div className="container">
       <h1>Welcome to the 6452 Land Title Management System</h1>
-      <div className="form-group">
-        <label htmlFor="contractAddress">Enter Contract Address:</label>
-        <input
-          type="text"
-          id="contractAddress"
-          value={contractAddress}
-          onChange={handleContractAddressChange}
-        />
-        <button onClick={handleContractAddressSubmit}>Submit</button>
+      <h2>There are <b>{numTokens}</b> Land Tokens currently minted</h2>
+      <div className="box" onClick={handleMerge}>
+        <h1>Merge</h1>
+      </div>
+      <div className="box" onClick={handleSplit}>
+        <div className="content">
+          <h1>Split</h1>
+        </div>
+      </div>
+      <div className="box" onClick={handleMint}>
+        <h1>Free tokens</h1>
       </div>
     </div>
   );
