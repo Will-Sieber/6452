14e32a223a38957e3779fea697c0256b38dd8aa1
IdolonInMachina <Cooperbar@gmail.com>
2023-07-25 13:29:27 +1000

    Fixes map not loading

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
- 
-mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+import { MAPBOX_KEY } from '../config'; 
+
+mapboxgl.accessToken = MAPBOX_KEY;
 
 
 
